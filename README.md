0aba4cf402c7d09cd67e9090ad7c556a32e21b5b
Curtis Brandt <curtisjbrandt@gmail.com>
2013-05-13 12:05:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight - Blah blah blah
+
+# SYNOPSIS
+
+    use POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight;
+
+# DESCRIPTION
+
+POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight is
+
+# AUTHOR
+
+Curtis Brandt <curtis@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Curtis Brandt
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/POE/Component/IRC/Plugin/WWW/KrispyKreme/HotLight.pm
@@ -0,0 +1,39 @@
+package POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight;
+
+=head1 DESCRIPTION
+
+POE::Component::IRC::Plugin::WWW::KrispyKreme::HotLight is
+
+=head1 AUTHOR
+
+Curtis Brandt E<lt>curtis@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Curtis Brandt
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
