af84de66f1749fe8058611c6ef0341114c760837
Curtis Brandt <curtis@cpan.org>
2014-01-17 09:24:34 -0800

    0.06

--- lib/POE/Component/IRC/Plugin/WWW/KrispyKreme/HotLight.pm
+++ lib/POE/Component/IRC/Plugin/WWW/KrispyKreme/HotLight.pm
@@ -5,7 +5,7 @@ use WWW::KrispyKreme::HotLight;
 use IRC::Utils qw(parse_user);
 use Carp::POE qw(croak);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use POE::Component::IRC::Plugin qw( :ALL );
 
